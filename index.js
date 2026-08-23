b03a7851b5253d628550fdff857b6bf0e1053d50
srinathstart <srinathyadav426@gmail.com>
2026-08-23 15:25:26 +0530

    feat: handle raw POST requests and JSON errors

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ const server = http.createServer((req, res) => {
         }));
     }
 
+    if (req.method === "POST" && req.url === "/projects") {
+    let body = "";
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 201;
+
+
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    req.on("end", () => {
+    try {
+        const project = JSON.parse(body);
+
+        res.statusCode = 201;
+
+        res.end(JSON.stringify({
+            message: "Project created",
+            project
+        }));
+    } catch (error) {
+        res.statusCode = 400;
+
+        res.end(JSON.stringify({
+            error: "Invalid JSON"
+        }));
+    }
+});
+
+
+    return;
+}
+
     res.statusCode = 404;
     res.end("Not Found");
 });
